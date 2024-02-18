91b395b8e715f496a489f663b8e47a9e808a7437
Dmitriy_Martys <12masek34@mail.ru>
2024-02-18 21:50:55 +0300

    add logger timestamp

--- config.py
+++ config.py
@@ -11,6 +11,11 @@ from dotenv import (
 
 load_dotenv()
 logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    format="%(asctime)s %(levelname)-8s %(message)s",
+    level=logging.INFO,
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
 log = logging.getLogger(__name__)
 
 pictures_dir = Path("static","pictures")
